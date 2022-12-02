6829b06590dbb7fc1aea0129c2800243da357149
derjogi <derjogi@web.de>
2022-12-03 11:25:46 +1300

    Advent of Code - Base & Day1

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+mod day1;
+mod utils;
+
+extern crate input_stream;
+
+use crate::day1::day1;
+
+fn main() {
+    day1()
+}
---
+++ src/utils.rs
@@ -0,0 +1,18 @@
+pub mod utils {
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
+    use std::path::Path;
+
+    pub fn parse_input(input: &str) -> Vec<&str> {
+        let lines: Vec<&str> = input.split("\n").collect();
+        lines
+    }
+
+    pub fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+        let file = File::open(Path::new("./src/").join(filename.as_ref())).expect("File not available?");
+        let reader = BufReader::new(file);
+        reader.lines()
+            .map(|l| l.expect("Eh? Could not parse this line :-("))
+            .collect()
+    }
+}
